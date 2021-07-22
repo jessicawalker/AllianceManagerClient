39c511a06d785b9f3eb2421701af87b64e8ca23f
Jessica Walker <jessicacrotty@gmail.com>
2021-07-22 00:47:56 -0400

    Converts component from class to function.

--- src/components/AllianceInfo.js
+++ src/components/AllianceInfo.js
@@ -1,151 +1,83 @@
-import React, { useRef, useState, Component } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Form, Button, Card, Alert } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
 //import AllianceService from "../services/services";
 import axios from "../axios";
 //import styles from './Auth.module.css'; 
 
-export default class AllianceInfo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            Alliances: []
-        };
-    }
-    getAllianceData() {
-        axios
-            .get(`/allianceprofile`, {})
-            .then(res => {
-                const data = res.data;
-                const labelStyle = {
-                    marginTop: '1em'
-                };
-                const btnStyle = {
-                    margin: '1em 0 0'
-                };
-                const hideStyle = {
-                    display: 'none'
-                };
-                
-                const alliances = data.results.map(u =>
-                    <div key={u._id}>
-                        <p style={hideStyle}>{u._id} - {u.alliance_name} - {u.game_name}</p>
-                        <Card>
-                            <Card.Body>
-                                <h2 className="text-center mb-4">Update Alliance</h2>
-                                <Form>
-                                    <Form.Group id="allianceName">
-                                        <Form.Label style={labelStyle}>Alliance Name</Form.Label>
-                                        <Form.Control type="text" required defaultValue={u.alliance_name} />
-                                    </Form.Group>
-                                    <Form.Group id="gameName">
-                                        <Form.Label style={labelStyle}>Game Name</Form.Label>
-                                        <Form.Control type="text" required defaultValue={u.game_name} />
-                                    </Form.Group>
-                                    <Button className="w-100" style={btnStyle} type="submit">
-                                        Update
-                                    </Button>
-                                </Form>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                )
-                
-                this.setState({
-                    alliances
-                })
-
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-
-    }
-    componentDidMount(){
-        this.getAllianceData()
-    }
-    render() {
-
-        return (
-            <>
-                <div>{this.state.alliances}</div>
-                
-                <div className="w-100 text-center mt-2">
-                    <Link to="/">Cancel</Link>
-                </div>
-            </>
-        )
-    }
-}
-
-/*
 export default function AllianceInfo() {
-    const emailRef = useRef()
-    const passwordRef = useRef()
-    const passwordConfirmRef = useRef()
-    const { currentUser, updatePassword, updateEmail } = useAuth()
-    const [error, setError] = useState('')
-    const [loading, setLoading] = useState(false)
-    const history = useHistory()
 
-    async function handleSubmit(e) {
-        e.preventDefault()
+    const [allianceData, setAllianceData] = useState({});
+    const labelStyle = {
+        marginTop: '1em'
+    };
+    const btnStyle = {
+        margin: '1em 0 0'
+    };
+    const hideStyle = {
+        display: 'none'
+    };
 
-        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-            return setError('Passwords do not match')
-        }
+    /*
+        axios.get('/allianceprofile', {})
+            .then(function (response) {
+                // handle success
+                const results = response.data.results[0];
+                //setAllianceData({_id: results._id, alliance_name: results.alliance_name, game_name: results.game_name});
+                console.log(allianceData);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });*/
 
-        const promises = []
-        setLoading(true)
-        setError('')
+    // read alliance data
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(
+                '/allianceprofile',
+            );
 
-        if (emailRef.current.value !== currentUser.email) {
-            promises.push(updateEmail(emailRef.current.value))
-        }
-        if (passwordRef.current.value) {
-            promises.push(updatePassword(passwordRef.current.value))
-        }
+            setAllianceData(result.data.results[0]);
+        };
 
-        Promise.all(promises)
-        .then(() => {
-            history.push("/")
-        })
-        .catch(() => {
-            setError("Failed to update account")
-        })
-        .finally(() => {
-            setLoading(false)
-        })
-    }
+        fetchData();
+    }, []);
+
+
+
 
     return (
-        <>
-            <Card>
-                <Card.Body>
-                    <h2 className="text-center mb-4">Update Profile</h2>
-                    { error && <Alert variant="danger">{ error }</Alert>}
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group id="email">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" ref={emailRef} required defaultValue={currentUser.email} />
-                        </Form.Group>
-                        <Form.Group id="password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" ref={passwordRef} placeholder="Leave blank to keep the same" />
-                        </Form.Group>
-                        <Form.Group id="password-confirm">
-                            <Form.Label>Password Confirmation</Form.Label>
-                            <Form.Control type="password" ref={passwordConfirmRef} placeholder="Leave blank to keep the same" />
-                        </Form.Group>
-                        <Button disabled={loading} className="w-100" type="submit">
-                            Update
-                        </Button>
-                    </Form>
-                </Card.Body>
-            </Card>
+        <div>
+            <div key={allianceData._id}>
+                <p style={hideStyle}>{allianceData._id} - {allianceData.alliance_name} - {allianceData.game_name}</p>
+                <Card>
+                    <Card.Body>
+                        <h2 className="text-center mb-4">Update Alliance</h2>
+                        <Form>
+                            <Form.Group id="allianceName">
+                                <Form.Label style={labelStyle}>Alliance Name</Form.Label>
+                                <Form.Control type="text" required defaultValue={allianceData.alliance_name} />
+                            </Form.Group>
+                            <Form.Group id="gameName">
+                                <Form.Label style={labelStyle}>Game Name</Form.Label>
+                                <Form.Control type="text" required defaultValue={allianceData.game_name} />
+                            </Form.Group>
+                            <Button className="w-100" style={btnStyle} type="submit">
+                                Update
+                            </Button>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            </div>
+
             <div className="w-100 text-center mt-2">
                 <Link to="/">Cancel</Link>
             </div>
-        </>
+        </div>
     )
-}*/
+
+}
