1386c9291a9ae95fe1c44494c93ef182aa3724d8
Jessica Walker <jessicacrotty@gmail.com>
2021-08-01 20:17:07 -0400

    Fixes navbar render and functionality for mobile.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,9 @@
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-import { Card, Button, Alert } from 'react-bootstrap';
+import { Container, Button, Alert, Nav, NavDropdown, Navbar } from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthContext';
 
-export default function Header() {
-    const [error, setError] = useState("");
-    const { currentUser, logout } = useAuth();
-    const history = useHistory();
-    const dashBtnStyle = {
-        backgroundColor: 'cornflowerblue',
-        color: 'white',
-        textDecoration: 'none'
-    };
-
-    async function handleLogout() {
-        setError('');
-
-        try {
-            await logout();
-            history.pushState('/logout');
-        } catch {
-            setError('Failed to log out');
-        }
-    }
-    
-    return (
+/*
         <header className="fixed-top bg-primary">
             <nav className="navbar navbar-expand-lg navbar-dark">
                 <Link className="navbar-brand" to="/"><img src="../imgs/logo-AM.png" alt="Alliance Manager" className="logo" /></Link>
@@ -43,6 +22,57 @@ export default function Header() {
                     <Button variant="link" style={dashBtnStyle} onClick={handleLogout}>Log Out</Button>
                 </div>
             </nav>
+        </header>*/
+
+export default function Header() {
+    const [error, setError] = useState("");
+    const { currentUser, logout } = useAuth();
+    const history = useHistory();
+    const dashBtnStyle = {
+        backgroundColor: 'cornflowerblue',
+        color: 'white',
+        textDecoration: 'none'
+    }
+
+    async function handleLogout() {
+        setError('');
+
+        try {
+            await logout();
+            history.pushState('/logout');
+        } catch {
+            setError('Failed to log out');
+        }
+    }
+
+    return (
+        <header className="fixed-top bg-primary">
+            <Navbar collapseOnSelect expand="lg" variant="dark">
+            <Container>
+                <Navbar.Brand href="/"><img src="../imgs/logo-AM.png" alt="Alliance Manager" className="logo" /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/activities">Activity Logs</Nav.Link>
+                        <Nav.Link href="/members">Members</Nav.Link>
+                        <NavDropdown title="Settings" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="/tracking-setup">Tracking Setup</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="/alliance-info">Alliance Profile</NavDropdown.Item>
+                            <NavDropdown.Item href="/update-profile">Account Profile</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav className="justify-content-end">
+                        <Nav.Link href="/tracking">
+                            <Button variant="primary" style={dashBtnStyle}>Add New Activity</Button>
+                        </Nav.Link>
+                        <Nav.Link>
+                            <Button onClick={handleLogout}>Log Out</Button>
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </header>
     )
 }
