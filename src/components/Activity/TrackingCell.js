b26a65d8835c1dce45fcc250176727f20e615d7e
Jessica Walker <jessicacrotty@gmail.com>
2021-08-02 20:45:19 -0400

    Adds code to generate rows and columns.
    At this point, the date comes from the date selected by the user at the top
    of the form. The members come from a get request already filtered to
    include current members only. The custom criteria comes from a get
    request of the tracking criteria.
    
    Most of the interface is complete, now needs to create and send the form
    data as an activity log entry.

--- src/components/Activity/TrackingCell.js
+++ src/components/Activity/TrackingCell.js
@@ -2,22 +2,24 @@ import React, { useRef, useState, useEffect } from 'react';
 import { Form, Button, Table, Card, Alert } from "react-bootstrap";
 /*import { Link, useHistory } from "react-router-dom";
 import axios from "../../axios";
-import RowTool from '../Row/RowTool';
-import styles from './activities.module.css';*/
+import RowTool from '../Row/RowTool';*/
+import styles from './activities.module.css';
 
 export default function TrackingCell(props) {
+    const [trackData, setTrackData] = useState(props.value);    // current members boolean
+
+
     return (
         <td>
-            {props.field === "date" && <Form.Control plaintext readOnly defaultValue={props.value} />}
-            {props.field === "user" && <Form.Control plaintext readOnly defaultValue={props.value} />}
-            {props.field === "criteria_datatype" && <Form.Control type="text" />}
-                {props.criteria_datatype === "String" && <Form.Control type="text" />}
-                {props.criteria_datatype === "Number" && <Form.Control type="number" />}
-                {props.criteria_datatype === "Boolean" && <Form.Control as="select" defaultValue="false">
-                    <option>true</option>
+            {props.field === "date" && <Form.Control plaintext readOnly defaultValue={trackData} />}
+            {props.field === "user" && <Form.Control plaintext readOnly defaultValue={trackData} />}
+                {props.field !== "user" && props.field !== "notes" && props.criteria_datatype === "String" && <Form.Control type="text" defaultValue={trackData} />}
+                {props.criteria_datatype === "Number" && <Form.Control type="number" defaultValue={trackData} />}
+                {props.criteria_datatype === "Boolean" && <Form.Control as="select" defaultValue={trackData}>
                     <option>false</option>
+                    <option>true</option>
                 </Form.Control>}
-                {props.criteria_datatype === "Date" && <Form.Control type="date" />}
+                {props.field !== "date" && props.criteria_datatype === "Date" && <Form.Control type="date" defaultValue={trackData} />}
             {props.field === "notes" && <Form.Control type="text" defaultValue={props.value} />}
         </td>
     )
--- src/components/Activity/TrackingLogList.js
+++ src/components/Activity/TrackingLogList.js
@@ -17,21 +17,22 @@ export default function TrackingLogList() {
     // -- use datatype to determine the type of field
 
     //const activityDate = new Date(Date.now()).toUTCString();
-    const [activityDate, setActivityDate] = useState(new Date(Date.now()).toUTCString());
+    const [activityDate, setActivityDate] = useState(new Date(Date.now()).toDateString());
     const [membersCurrent, setMembersCurrent] = useState(true);    // current members boolean
     const [memberData, setMemberData] = useState([{}]);    // members list
+    const [memberCurrentArray, setMemberCurrentArray] = useState([]);    // members list
     const [criteriaData, setCriteriaData] = useState([{}]);    // tracking criteria list
     const [memberActivityDataDate, setActivityDataDate] = useState([{ date: activityDate }]); //separate date out?
     const [memberActivityDataNotes, setActivityDataNotes] = useState([{ notes: "" }]);  // separate notes out?
     const [memberActivityData, setActivityData] = useState([{}]);  // master array each user
-    //const headingArray = [];
+    const [notesEntry, setNotesEntry] = useState([]);
     const [headingArray, setHeadingArray] = useState([]);    // tracking criteria list
     let history = useHistory();
 
     // read current members
     // change so that the search for current vs all is passed as
     //      params, not set in stone in back-end?
-    useEffect(() => {
+    /*useEffect(() => {
         const fetchData = async () => {
             const result = await axios.get('/members', {
                 params: {
@@ -43,7 +44,7 @@ export default function TrackingLogList() {
         };
 
         fetchData();
-    }, []);
+    }, []);*/
 
     useEffect(() => {
         const fetchData = async () => {
@@ -51,10 +52,14 @@ export default function TrackingLogList() {
                 '/members-current',
             );
             setMemberData(result.data.members);
-        //console.log(result.data.members);
+        console.log(result.data.members);
         };
 
+        setMemberCurrentArray(memberData.values());
         fetchData();
+        for (const value of memberCurrentArray) {
+            console.log(value);
+          }
     }, []);
 
     // read tracking criteria
@@ -125,24 +130,66 @@ export default function TrackingLogList() {
 
     //headingArray = {criteriaData.criteria_name};
 
-    console.log(memberData);
+    //console.log(memberData);
+
+    function generateUserdataMatrix(activityDate, memberData, criteriaData) {
+        for (let x = 0; x < memberData.length; x++) {
+            // make matrix
+            // date, member[x], criteria, notes
+        }
+
+    }
 
     async function handleSubmit(e) {
         e.preventDefault();/*
-        { "msg": "SUCCESS", "members": [{ "_id": "60fb2d9d145d57236c7cce6e", "member_username": "Test Member", "member_role": "Tester", "member_notes": "TBD", "current_member": true, "member_added_date": "2021-04-09T04:00:00.000Z", "__v": 0 }, { "_id": "60fe86c68cbbdc8ef0a88e5e", "member_username": "Tester User", "member_role": "test", "member_notes": "Testing", "current_member": true, "member_added_date": "Mon, 26 Jul 2021 09:56:22 GMT" }, { "_id": "6105c9e3c53b4b4bec5f6487", "member_username": "Newest Member", "member_role": "", "member_notes": "", "current_member": true, "member_added_date": "Sat, 31 Jul 2021 22:08:35 GMT" }] }*/
+        { "msg": "SUCCESS", "members": [{ "_id": "60fb2d9d145d57236c7cce6e", "member_username": "Test Member", "member_role": "Tester", "member_notes": "TBD", "current_member": true, "member_added_date": "2021-04-09T04:00:00.000Z", "__v": 0 }, 
+        { "_id": "60fe86c68cbbdc8ef0a88e5e", "member_username": "Tester User", "member_role": "test", "member_notes": "Testing", "current_member": true, "member_added_date": "Mon, 26 Jul 2021 09:56:22 GMT" }, 
+        { "_id": "6105c9e3c53b4b4bec5f6487", "member_username": "Newest Member", "member_role": "", "member_notes": "", "current_member": true, "member_added_date": "Sat, 31 Jul 2021 22:08:35 GMT" }] }*/
     }
 
-    return (
-        <Form onSubmit={handleSubmit}>
+    // array => 
+    // * date - from date field located at top of form, defaulting to today
+    // * user - from memberData.member_username
+    // * criteria - from criteriaData.{...}
+    // * notes - all new
 
-            <Form.Control type="date" defaultValue={activityDate} />
+    // test array of values
+    const testArrayFields = ["date", "user", "claimedSSwar", "activeDeclare", "defenseEarly", "defenselive", "offense", "notes"];
+    const testArrayValues = [activityDate, "Testing", "", true, false, true, false, "none"];
+    const testArrayKeyValuePairs = {"date": activityDate, "user": "Testing", "claimedSSwar": "", "activeDeclare": true, "defenseEarly": false, "defenselive": true, "offense": false, "notes": "none"};
+    const testMatrix = [
+        {"date": activityDate, "user": "Testing", "claimedSSwar": "", "activeDeclare": true, "defenseEarly": false, "defenselive": true, "offense": false, "notes": "none"}, 
+        {"date": activityDate, "user": "Second Member", "claimedSSwar": true, "activeDeclare": false, "defenseEarly": false, "defenselive": true, "offense": true, "notes": ""}, 
+        {"date": activityDate, "user": "Last Member", "claimedSSwar": true, "activeDeclare": true, "defenseEarly": true, "defenselive": false, "offense": false, "notes": "joined today"}
+    ];
+    //var iterator = testArrayKeyValuePairs.values();
+    var iterator = testMatrix.values();
+    //console.log(memberCurrentArray);
+    /*
+    
+                    {criteriaData.map((criteria) => (
+                        <TrackingCell
+                            key={criteria._id}
+                            field={criteria.criteria_key}
+                            value={iterator.next().value}
+                            criteria_datatype={criteria.criteria_datatype}
+                        />))}
+                        */
+
+    return (
+        <Form onSubmit={handleSubmit} className={styles.formFormat}>
+
+            <Form.Control type="date" as="input" className="text-center me-auto ms-auto" required defaultValue={activityDate} onChange={(e) => setActivityDate(e.target.value)} />
 
             <Table className="table" striped bordered hover responsive="md">
                 <thead>
                     <tr>
+                        <th>Date</th>
+                        <th>User</th>
                         {criteriaData.map((criteria) => (
                             <th>{criteria.criteria_name}</th>
                         ))}
+                        <th>Notes</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -150,62 +197,82 @@ export default function TrackingLogList() {
                         <TrackingCell
                             key="date123"
                             field="date"
-                            value=""
+                            value={testArrayValues[0]}
                             criteria_datatype=""
                         />
                         <TrackingCell
                             key="user123"
                             field="user"
-                            value="Test"
+                            value={testArrayValues[1]}
                             criteria_datatype=""
                         />
                         <TrackingCell
                             key="test123"
                             field=""
-                            value=""
+                            value={testArrayValues[2]}
                             criteria_datatype="Boolean"
                         />
                         <TrackingCell
                             key="test234"
                             field=""
-                            value=""
+                            value={testArrayValues[3]}
                             criteria_datatype="Boolean"
                         />
                         <TrackingCell
                             key="test345"
                             field=""
-                            value={true}
+                            value={testArrayValues[4]}
                             criteria_datatype="Boolean"
                         />
                         <TrackingCell
                             key="test456"
                             field=""
-                            value=""
+                            value={testArrayValues[5]}
                             criteria_datatype="Boolean"
                         />
                         <TrackingCell
                             key="test567"
                             field=""
-                            value=""
+                            value={testArrayValues[6]}
                             criteria_datatype="Boolean"
                         />
                         <TrackingCell
                             key="notes123"
                             field="notes"
-                            value=""
+                            value={testArrayValues[7]}
                             criteria_datatype=""
                         />
                     </tr>
-                    <tr>
-                        
-                    {criteriaData.map((criteria) => (
-                        <TrackingCell
-                            key={criteria._id}
-                            field={criteria.criteria_key}
-                            value="test"
-                            criteria_datatype={criteria.criteria_datatype}
-                        />))}
-                    </tr>
+                    {memberData.map((member) => (
+                        <tr>
+                            <TrackingCell
+                                key={member}
+                                field="date"
+                                value={activityDate}
+                                criteria_datatype="Date"
+                            />
+                            <TrackingCell
+                                key={member}
+                                field="user"
+                                value={member.member_username}
+                                criteria_datatype="String"
+                            />
+                            
+                            {criteriaData.map((criteria) => (
+                                <TrackingCell
+                                    key={criteria._id}
+                                    field={criteria.criteria_key}
+                                    value={iterator.next().value}
+                                    criteria_datatype={criteria.criteria_datatype}
+                                />))}
+
+                            <TrackingCell
+                                key={member}
+                                field="notes"
+                                value={notesEntry}
+                                criteria_datatype="String"
+                            />
+                        </tr>))}
                 </tbody>
             </Table>
             {memberActivityData.length === 0 && <h3>Error</h3>}
