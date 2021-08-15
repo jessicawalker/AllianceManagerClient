95a184baf778373756e71aa901455a5853d4b401
Jessica Walker <jessicacrotty@gmail.com>
2021-08-14 20:39:46 -0400

    Style updates to dashboard.

--- src/components/Dash/DashTool.js
+++ src/components/Dash/DashTool.js
@@ -3,21 +3,12 @@ import { Link } from 'react-router-dom';
 import styles from './Dash.module.css';
 
 export default function DashTool(props) {
-    const dashImgStyle = {
-        float: 'left',
-        height: '1.4em', 
-        margin: '0.2em 0.5em 0 0',
-        width: '1.4em'
-    };
-    const dashLinkStyle = {
-        textDecoration: 'none'
-    };
 
     return (
-        <div>
-            <Link to={`${props.goto}`} style={dashLinkStyle}>
-                <img src={`../../imgs/${props.img}.png`} alt={`${props.pageName}`} style={dashImgStyle} />
-                <h4>{props.pageName}</h4>
+        <div className={styles.dashToolItem}>
+            <Link to={`${props.goto}`} className={styles.dashLinkStyle}>
+                <img src={`../../imgs/${props.img}.png`} alt={`${props.pageName}`} className={styles.dashImg} />
+                <h3>{props.pageName}</h3>
             </Link>
         </div>
     )
--- src/components/Dash/Dashboard.js
+++ src/components/Dash/Dashboard.js
@@ -9,10 +9,6 @@ export default function Dashboard() {
     const [error, setError] = useState("");
     const { currentUser, logout } = useAuth();
     const history = useHistory();
-    const dashBtnStyle = {
-        color: 'black',
-        textDecoration: 'none'
-    };
 
     async function handleLogout() {
         setError('');
@@ -31,16 +27,18 @@ export default function Dashboard() {
                 <Card.Body>
                     <h2 className="text-center mb-4">Manager Tools</h2>
                     { error && <Alert variant="danger">{ error }</Alert>}
-                    <DashTool goto="/tracking" img="plus" pageName="Add New Activity" />
-                    <DashTool goto="/activities" img="edit" pageName="Activity Logs" />
-                    <DashTool goto="/members" img="multiple-users-silhouette" pageName="Members" />
-                    <DashTool goto="/tracking-setup" img="settings" pageName="Tracking Setup" />
-                    <DashTool goto="/alliance-info" img="gamepad" pageName="Alliance Profile" />
-                    <DashTool goto="/update-profile" img="user" pageName="Account Profile" />
+                    <div className={styles.cardContainer}>
+                        <DashTool goto="/tracking" img="plus" pageName="Add New Activity" />
+                        <DashTool goto="/activities" img="edit" pageName="Activity Logs" />
+                        <DashTool goto="/members" img="multiple-users-silhouette" pageName="Members" />
+                        <DashTool goto="/tracking-setup" img="settings" pageName="Tracking Setup" />
+                        <DashTool goto="/alliance-info" img="gamepad" pageName="Alliance Profile" />
+                        <DashTool goto="/update-profile" img="user" pageName="Account Profile" />
+                    </div>
                 </Card.Body>
             </Card>
             <div className="w-100 text-center mt-2">
-                <Button variant="link" style={dashBtnStyle} onClick={handleLogout}>Log Out</Button>
+                <Button variant="link" className={styles.dashBtnStyle} onClick={handleLogout}>Log Out</Button>
             </div>
         </div>
     )
