7dc96e36a642e1d1efa8f8cd5ef13140951990d6
Jessica Walker <jessicacrotty@gmail.com>
2021-09-25 17:03:39 -0400

    Adds test API on Heroku.

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:2000/",
+    //baseURL: "http://localhost:2000/",
+    baseURL: "https://alliance-manager.herokuapp.com/",
     headers: {
         "Content-type": "application/json"
     }
